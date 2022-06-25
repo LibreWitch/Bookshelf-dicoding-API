af5e7103c1a7c6579a59467af59bdbe5a045ad2a
librewitch <amroslayer@gmail.com>
2022-06-25 10:41:22 +0700

    fix bugs (i guess)

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: '0.0.0.0',
+    port: 3000,
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
