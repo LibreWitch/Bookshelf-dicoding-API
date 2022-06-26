6ec10123bf4a0d75a52acd23e39dffc65ae6a390
librewitch <amroslayer@gmail.com>
2022-06-26 12:23:56 +0700

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -3,12 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-<<<<<<< HEAD
     port: 5000,
     host: 'localhost',
-=======
-    port: 3000,
->>>>>>> 0533736848785d0ec7cdec4fcf7345c745d2a159
     routes: {
       cors: {
         origin: ['*'],
@@ -19,7 +15,7 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
- // console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
